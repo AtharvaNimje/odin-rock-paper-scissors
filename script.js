6e248d280c806543e92b08be2787ebe7c877c568
Atharva Nimje <atharvanimje@gmail.com>
2022-05-30 11:38:51 +0530

    add playRound() function

--- script.js
+++ script.js
@@ -4,4 +4,40 @@ function computerPlay() {
     return rps_arr[ rand_index ]
 }
 
-console.log(computerPlay());
+//console.log(computerPlay());
+
+function playRound( playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if (playerSelection === "Rock"){
+        if (computerSelection === "Rock") {
+            return "draw";
+        }
+        if (computerSelection === "Paper") {
+            return "computer";
+        }
+        if (computerSelection === "Scissors") {
+            return "player";
+        }
+    } else if (playerSelection === "Paper") {
+        if (computerSelection === "Rock") {
+            return "player";
+        }
+        if (computerSelection === "Paper") {
+            return "draw";
+        }
+        if (computerSelection === "Scissors") {
+            return "computer";
+        }
+    } else if (playerSelection === "Scissors") {
+        if (computerSelection === "Rock") {
+            return "computer";
+        }
+        if (computerSelection === "Paper") {
+            return "player";
+        }
+        if (computerSelection === "Scissors") {
+            return "draw";
+        }
+    }
+}

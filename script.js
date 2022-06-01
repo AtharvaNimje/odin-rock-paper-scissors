8031209fe3fc023d081cd9eb5a45575bd84a7eb3
Atharva Nimje <atharvanimje@gmail.com>
2022-06-01 15:17:26 +0530

    Add UI to the RPS game

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function computerPlay() {
     let rps_arr = ["Rock", "Paper", "Scissors"];
     rand_index = Math.floor(Math.random() * rps_arr.length);
-    return rps_arr[ rand_index ]
+    return rps_arr[rand_index]
 }
 
 //console.log(computerPlay());
 
-function playRound( playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
 
@@ -14,7 +14,7 @@ function playRound( playerSelection, computerSelection) {
 
     if (playerSelection === computerSelection) {
         return "draw";
-    }else if (
+    } else if (
         ((playerSelection === "rock") && (computerSelection === "scissors")) ||
         ((playerSelection === "paper") && (computerSelection === "rock")) ||
         ((playerSelection === "scissors") && (computerSelection === "paper"))
@@ -30,31 +30,53 @@ function playRound( playerSelection, computerSelection) {
 }
 
 function game() {
-    let player = 0, computer = 0;
+    let player_score = 0, computer_score = 0;
+    const buttons = document.querySelectorAll('button');
 
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Please enter your choice: ");
-        let computerSelection = computerPlay();
-        winner = playRound(playerSelection, computerSelection);
+    buttons.forEach((button => {
+        button.addEventListener('click', () => {
+            const playerSelection = button.id;
+            let computerSelection = computerPlay();
+            const winner = playRound(playerSelection, computerSelection);
 
-        console.log(`Winner of Round ${i+1}: ${winner}`);
+            console.log(`Winner of this Round: ${winner}`);
 
-        if (winner === "player") {
-            player += 1;
-        } else if (winner === "computer") {
-            computer += 1;
-        }
-    }
+            if (winner === "player") {
+                player_score += 1;
+            } else if (winner === "computer") {
+                computer_score += 1;
+            }
+            console.log(player_score, computer_score);
 
-    console.log(player, computer);
+            const displayDiv = document.querySelector('.score');
+            displayDiv.textContent = `Player: ${player_score} Computer: ${computer_score}`;
 
-    if (player > computer) {
-        console.log("The final winner is player!");
-    } else if(computer > player) {
-        console.log("The final winner is computer!");
-    } else {
-        console.log("It's a draw!")
-    }
+            if (player_score === 5 || computer_score === 5) {
+                document.querySelector('.rpsBtn').style.display = 'none';
+                if (player_score > computer_score) {
+                    displayDiv.textContent = 'Player won! Do you want to play again?'
+                    resetGame();
+                } else {
+                    displayDiv.textContent = 'Computer won! Do you want to try again?'
+                    resetGame();
+                }
+            }
+        })
+    }));
+
+
+}
+
+function resetGame() {
+    tryAgainBtn = document.createElement('button');
+    tryAgainBtn.classList.add('tryAgainBtn');
+    tryAgainBtn.textContent = 'Try again?';
+    const displayDiv = document.querySelector('.score');
+    displayDiv.appendChild(tryAgainBtn);
+
+    tryAgainBtn.addEventListener('click', function() {
+        window.location.reload()
+    });
 }
 
 game();
